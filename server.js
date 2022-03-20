2a9dcd7a19b468c80daf2a8543d09ac2b9a4ce2e
JHessler <jhessler11@gmail.com>
2022-03-20 10:15:24 -0400

    cleaned up code and added notes

--- server.js
+++ server.js
@@ -6,22 +6,26 @@ const db = require('./db/db.json')
 
 const PORT = process.env.PORT || 3001;
 
-
+// handles post request 
 app.use(express.json());
 app.use(express.urlencoded( {
     extended: true 
 }));
+
+// handles in coming js,css, and images
 app.use(express.static('public'));
 
+// get index.html file
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'))
 });
 
+// get notes.html
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'))
 });
 
-
+// runs the server
 app.listen(PORT, () => {
     console.log(`API server now on ${PORT}!`)
 })
