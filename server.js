9009a774aa6be03aed5dbe10fd519389617a3942
JHessler <jhessler11@gmail.com>
2022-03-20 10:05:47 -0400

    fixed unexpected token bug

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express()
 const path = require('path');
 const db = require('./db/db.json')
-const notes = require('./public/notes.html')
+
 
 const PORT = process.env.PORT || 3001;
 
